435c91d285fa7f2f9f4b42354d4e0c949a59b444
QGdev <quentin.gomes.dev@gmail.com>
2022-03-03 16:29:19 +0100

    Add JavaDoc in PlaceAlreadyExistException.java and PlaceDoesntExistException.java

--- app/src/main/java/fr/qgdev/openweather/dataplaces/PlaceAlreadyExistException.java
+++ app/src/main/java/fr/qgdev/openweather/dataplaces/PlaceAlreadyExistException.java
@@ -1,8 +1,23 @@
 package fr.qgdev.openweather.dataplaces;
 
-//  A simple class to identify exception due to a place already present in storage
+/**
+ * PlaceAlreadyExistException
+ * <p>
+ * A simple class to identify exception due to a place already present in storage
+ * </p>
+ *
+ * @author Quentin GOMES DOS REIS
+ * @version 1
+ * @see Exception
+ */
 public class PlaceAlreadyExistException extends Exception {
 
+	/**
+	 * PlaceAlreadyExistException Constructor
+	 * <p>
+	 * Just the constructor of PlaceAlreadyExistException class
+	 * </p>
+	 */
 	public PlaceAlreadyExistException() {
 		super("Cannot add a place that already present in storage");
 	}
--- app/src/main/java/fr/qgdev/openweather/dataplaces/PlaceDoesntExistException.java
+++ app/src/main/java/fr/qgdev/openweather/dataplaces/PlaceDoesntExistException.java
@@ -1,9 +1,24 @@
 package fr.qgdev.openweather.dataplaces;
 
-//  A simple class to identify exception due to a place doesn't exist in storage
+/**
+ * PlaceDoesntExistException
+ * <p>
+ * A simple class to identify exception due to a place doesn't exist in storage
+ * </p>
+ *
+ * @author Quentin GOMES DOS REIS
+ * @version 1
+ * @see Exception
+ */
 public class PlaceDoesntExistException extends Exception {
 
+	/**
+	 * PlaceDoesntExistException Constructor
+	 * <p>
+	 * Just the constructor of PlaceDoesntExistException class
+	 * </p>
+	 */
 	public PlaceDoesntExistException() {
-		super("Cannot save a place data that isn\'t present in storage");
+		super("Cannot save a place data that isn't present in storage");
 	}
 }
