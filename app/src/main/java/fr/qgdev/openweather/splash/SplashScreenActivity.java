34551b823a882b2ae28d00770f830fc9fd7d0716
QGdev <quentin.gomes.dev@gmail.com>
2023-06-07 16:21:48 +0200

    Refactoring to avoid the use of Handler deprecated constructor in SplashScreenActivity.java

--- app/src/main/java/fr/qgdev/openweather/splash/SplashScreenActivity.java
+++ app/src/main/java/fr/qgdev/openweather/splash/SplashScreenActivity.java
@@ -3,6 +3,7 @@ package fr.qgdev.openweather.splash;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,17 +17,16 @@ public class SplashScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
-
-        int SPLASH_TIME_OUT = 1000;
-        new Handler().postDelayed(() -> {
+    
+        int splashTimeOut = 1000;
+        new Handler(Looper.getMainLooper()).postDelayed(() -> {
             // This method will be executed once the timer is over
             // Start your app main activity
             Intent mainActivity = new Intent(SplashScreenActivity.this, MainActivity.class);
             startActivity(mainActivity);
-
+        
             // close this activity
             finish();
-        }, SPLASH_TIME_OUT);
-
+        }, splashTimeOut);
     }
 }
